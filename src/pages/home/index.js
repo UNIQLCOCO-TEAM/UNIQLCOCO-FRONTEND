eba417dcbee4cc48dcf4f232c54125ba354b1bc5
sittipak <sittipak.ssw@gmail.com>
2024-04-11 14:19:36 +0700

    edit bug in homepage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -12,12 +12,12 @@ export default function Home() {
       <div className="relative">
         <img src="/Home.png" className="w-full h-auto" alt="Background Image" />
 
-        <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-lg md:text-2xl lg:text-4xl xl:text-5xl font-bold text-white mt-7 md:mt-9 lg:mt-24 xl:mt-32">
+        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-lg md:text-2xl lg:text-4xl xl:text-5xl font-bold text-white mt-12 md:mt-24 lg:mt-32 xl:mt-32">
           WELCOME TO
         </div>
 
         <img
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-16 md:h-20 lg:h-24 xl:h-32 w-auto mt-0 md:mt-0 lg:mt-15 xl:mt-15"
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-10 md:h-14 lg:h-24 xl:h-32 w-auto mt-2 md:mt-2 lg:mt-15 xl:mt-15"
           src="/LOGO1.png"
           alt="Your Company"
         />
@@ -28,7 +28,7 @@ export default function Home() {
       </div>
 
       <div className="bg-white">
-        <div className="pl-10 pt-16 pb-10 md:pl-9 lg:pl-24 xl:pl-32 md:pb-5 lg:pb-10 xl:pb-10 text-xl md:text-xl lg:text-xxl xl:text-xxxl font-bold text-green-1">POPULAR</div>
+        <div className="pl-10 pt-16 pb-10 md:pl-9 lg:pl-24 xl:pl-32 md:pb-5 lg:pb-10 xl:pb-10 text-xl md:text-xl lg:text-xxl xl:text-xxxl font-bold text-green1">POPULAR</div>
       </div>
       <div className="bg-white px-8">
         <ProductList />
